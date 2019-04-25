611c07b88a405803c5ff55d273a0ebfb4cba2f21
Tom Young <tyoung@n.io>
2019-04-25 13:29:48 -0600

    birbs

--- pages/home.js
+++ pages/home.js
@@ -1,65 +1,124 @@
-import React, { Component } from 'react';
-import { Button, Chart, CircularProgressBar, Loader } from '@nio/ui-kit';
-import { withPubkeeper } from '../providers/pubkeeper';
+import React, { Component } from "react";
+import { Chart, Progress } from '@nio/ui-kit';
+import { withPubkeeper } from "../providers/pubkeeper";
 
 class Page extends Component {
-  state = { rand_num: 0, state_color: '', history: false };
+  state = { state: "", image: "", predictions: [], tally: {}, timestamp: "" };
 
   componentDidMount = () => {
     const { pkClient } = this.props;
-    pkClient.addPatron('rand_num', patron => { 
-      patron.on('message', this.writeDataToState);
+    pkClient.addPatron("birds.capture", patron => { 
+      patron.on("message", this.writeDataToState);
       return () => {
         // deactivation/tear-down
         patron.off("message", this.writeDataToState);
       };
-    } );
-    pkClient.addBrewer('button', brewer => {
-        this.brewer = brewer;
-        return () => null;  
-    } );
+    });
+    pkClient.addPatron("birds.tally", patron => { 
+      patron.on("message", this.writeDataToState);
+      return () => {
+        // deactivation/tear-down
+        patron.off("message", this.writeDataToState);
+      };
+    });
   };
 
-  writeDataToState = (data) => {
-    const json = new TextDecoder().decode(data);
-    const { rand_num, state_color, history } = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
-    this.setState({ rand_num, state_color, history });
+  writeDataToState = (rawData) => {
+    const newState = this.parseData(rawData);
+    this.setState(newState);
   };
 
-  brewCurrentTimestamp = () => {
-    this.brewer.brewJSON([{ timestamp: new Date() }]);
+  parseData = (rawData) => {
+    const rawJson = new TextDecoder().decode(rawData);
+    const json = JSON.parse(rawJson);
+    const parsedJson = json[json.length-1]
+    parsedJson.timestamp = this.getTime(parsedJson.timestamp);
+    return parsedJson;
+  };
+
+  getTime = (timestamp) => {
+    var localTime = new Date(timestamp);
+    var localTime = localTime.toLocaleString(
+      "en-US", { 
+        weekday: "long",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true
+      }
+    );
+    return localTime;
+  };
+
+  speciesName = (rawName) => {
+    const openParen = rawName.lastIndexOf("(");
+    const closeParen = rawName.lastIndexOf(")");
+    const commonName = rawName.slice(openParen + 1, closeParen);
+    const latinName = rawName.slice(0, openParen - 1);
+    return {
+      "commonName": commonName,
+      "latinName": latinName
+    };
+  };
+
+  percentScore = (score) => {
+    return Math.trunc(score * 1000) / 10;
   };
 
   render() {
-    const { rand_num, state_color, history } = this.state;
-    console.log(this.state.history);
+    const { state, image, predictions, tally, timestamp } = this.state;
+    const { commonName, latinName } = this.speciesName(state);
     return (
       <center>
-      <table cellspacing="10">
-      <tbody>
-      <tr valign='top'>
-      <td width='25%'>
-      <CircularProgressBar percentage={rand_num} color={state_color} />
-      </td>
-      <td rowSpan='2' className='chartcell'>
-      {history ? ( <Chart
-        title='Historical Values'
-        type='line'
-        data={
-            {'labels':['','','','','','','','','',''], 'datasets':[{'values': history}]}}
-      /> ) : ( <Loader /> )}
-      </td>
-      </tr>
-      <tr>
-      <td>
-      <Button block color="primary" onClick={() => this.brewCurrentTimestamp()}>random number!</Button>
-      </td>
-      </tr>
-      </tbody>
-      </table>
+        <table cellSpacing="10" border="1"  width="100%">
+          <tbody>
+            <tr valign="top">
+              <td width="50%" rowSpan="6">
+                <img src={ `data:image/jpeg;base64,${image}` } width="100%" />
+              </td>
+              <td>
+                <h2>{ commonName }</h2>
+                <h5><i>{ latinName }</i></h5>
+                Sighted: { timestamp }
+                <hr className="dashed" /> 
+              </td>
+            </tr>
+            {predictions && predictions.map(p => (
+              <tr key={p.label}>
+                <td>
+                  <Progress value={this.percentScore(p.score)} />
+                  {
+                    this.speciesName(p.label).commonName
+                  }: {
+                    this.percentScore(p.score)
+                  }%
+                </td>
+              </tr> 
+            ))}
+          </tbody>
+        </table>
+        <br />
+        <table cellSpacing="10" border="1" width="100%">
+          <tbody>
+            <tr>
+              <td>
+                <Chart type="pie" data={{
+                  "datasets": [
+                    {
+                      "name": "dem birbs",
+                      "chartType": "line",
+                      "values": Object.values(tally)
+                    }
+                  ],
+                  "labels": Object.keys(tally)
+                }}/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
       </center>
     );
-  }
-}
+  };
+};
 
 export default withPubkeeper(Page);
